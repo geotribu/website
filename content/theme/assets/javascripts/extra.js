3259bc42324d6ef16c3f536e07af139ec46f3383
Julien <Guts@users.noreply.github.com>
2020-08-17 17:32:03 +0200

    Fix iframe-worker path

--- content/theme/assets/javascripts/extra.js
+++ content/theme/assets/javascripts/extra.js
@@ -2,7 +2,7 @@
 var links = document.links;
 
 for (var i = 0, linksLength = links.length; i < linksLength; i++) {
-    if ((links[i].hostname != window.location.hostname) && !("data-mediabox" in t.attributes)) {
+    if ((links[i].hostname != window.location.hostname) && !("data-mediabox" in links[i].attributes)) {
         links[i].target = '_blank';
     }
 }
