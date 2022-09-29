92a75f920ecbc512159022473f2c6b4ce1a1c7de
GeoJulien <dev@ingeoveritas.com>
2022-09-29 14:36:32 +0200

    Add input config folder option and use mkdocs loader

--- scripts/mkdocs_config_merger.py
+++ scripts/mkdocs_config_merger.py
@@ -1,15 +1,25 @@
+#! python3  # noqa: E265
+
+"""
+    Description: Merge different mkdocs configuration files from a directory.
+    Author: Julien Moura (@Guts@github.com)
+
+    Requires: MkDocs installed (it uses its YAML loader)
+"""
+
+# -- IMPORTS --
+
+# standard lib
 import argparse
 from pathlib import Path
 
+# 3rd party
 import yaml
-from yaml_env_tag import construct_env_tag
+from mkdocs.utils import yaml_load
 
-# GLOBALS
-yaml.Loader.add_constructor("!ENV", construct_env_tag)
-
-# CLI
+# -- CLI --
 parser = argparse.ArgumentParser(
-    prog="MkDocsConfigMerger", description="Merge configuration files."
+    prog="MkDocsConfigMerger", description="Merge configuration files.", add_help=True
 )
 parser.add_argument(
     "-c",
@@ -19,20 +29,35 @@ parser.add_argument(
     help="Path to the configuration file to complete. Must exist.",
     default="mkdocs.yml",
 )
+parser.add_argument(
+    "-i",
+    "--input-folder",
+    dest="input_config_folder",
+    type=Path,
+    help="Path to the folder where to load configurations files to merge. Must exist.",
+    default="./config",
+)
+
 args = parser.parse_args()
 
 output_config_file = args.output_config_file
+input_config_folder = args.input_config_folder
 
-# check files exist
+# -- CHECKS --
+
 if not output_config_file.is_file():
     raise FileNotFoundError(output_config_file)
+if not input_config_folder.is_dir():
+    raise FileNotFoundError(input_config_folder)
+
+# -- MAIN --
 
 # list files to append
-configs_to_merge = Path("config/").glob("*.yml")
+configs_to_merge = input_config_folder.glob("*.yml")
 
 # load final config
 with output_config_file.open(mode="r", encoding="UTF8") as in_yaml:
-    config_to_complete = yaml.load(in_yaml, Loader=yaml.Loader)
+    config_to_complete = yaml_load(in_yaml)
 
 for cfg_file in configs_to_merge:
     dest_section = cfg_file.stem.split("_")[0]
@@ -45,4 +70,10 @@ for cfg_file in configs_to_merge:
 
 # write merged final config file
 with output_config_file.open("w") as out_file:
-    yaml.dump(config_to_complete, out_file, sort_keys=True, default_flow_style=False)
+    yaml.dump(
+        config_to_complete,
+        out_file,
+        sort_keys=True,
+        default_flow_style=False,
+        encoding="UTF8",
+    )
