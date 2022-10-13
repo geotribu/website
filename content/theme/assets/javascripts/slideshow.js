61f51b073d3cbe4cb3905425f3b2c576bb1d4056
GeoJulien <dev@ingeoveritas.com>
2022-10-13 17:51:39 +0200

    Rm useless assets

--- content/theme/assets/javascripts/slideshow.js
+++
@@ -1,9 +0,0 @@
-new Glider(document.querySelector('.glider'), {
-    slidesToShow: 1,
-    dots: '#dots',
-    draggable: true,
-    arrows: {
-      prev: '.glider-prev',
-      next: '.glider-next'
-    }
-  });
