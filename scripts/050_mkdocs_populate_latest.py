60794e26295749bd66b0f2241b8b730a14544da9
GeoJulien <dev@ingeoveritas.com>
2023-07-27 00:41:37 +0200

    Corrige le / manquant

--- scripts/050_mkdocs_populate_latest.py
+++ scripts/050_mkdocs_populate_latest.py
@@ -31,7 +31,7 @@ logger = logging.getLogger("mkdocs")
 def get_latest_content(
     content_type: Literal["articles", "rdp"],
     count: int = 10,
-    social_card_image_base: str = "https://static.geotribu.fr/assets/images/social",
+    social_card_image_base: str = "https://static.geotribu.fr/assets/images/social/",
 ):
     output_contents_list: list[Page] = []
 
@@ -72,7 +72,7 @@ output_dict = {"latest": {"articles": [], "rdp": []}}
 for k in output_dict.get("latest"):
     output_dict["latest"][k] = get_latest_content(
         content_type=k,
-        social_card_image_base=f"{mkdocs_config.get('site_url')}assets/images/social",
+        social_card_image_base=f"{mkdocs_config.get('site_url')}assets/images/social/",
     )
 
 with Path("config/extra_latest.yml").open("w", encoding="UTF-8") as out_file:
