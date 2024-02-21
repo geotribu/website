7af7353766434d3d500e2c40e61ddb771c818f67
GeoJulien <dev@ingeoveritas.com>
2024-02-21 15:17:52 +0100

    script: version with python-frontmatter

--- scripts/cleanup/frontmatter_date_str_to_date.py
+++ scripts/cleanup/frontmatter_date_str_to_date.py
@@ -1,28 +1,68 @@
-import re
+#! python3  # noqa: E265
+
+"""Script meant to be used as one-shot to convert YAML fonrtmatter 'date' value from
+    str into date.
+
+See: https://github.com/mkdocs/mkdocs/discussions/3576
+
+"""
+
+# -- IMPORTS --
+
+# standard
+import logging
+from datetime import date, datetime
 from pathlib import Path
 
+# 3rd party
+import frontmatter
+
+# -- MAIN --
+
+dt_format = "%Y-%m-%d %H:%M"
+logging.basicConfig(level=logging.INFO)
+
+# match = re.search(
+#     r"^---\s*\n.*?date: (\d{4}-\d{2}-\d{2}).*?\n---",
+#     content,
+#     re.DOTALL | re.MULTILINE,
+# )
+
 for md_filepath in Path("content").glob("**/*.md"):
-    with md_filepath.open(mode="r", encoding="UTF-8") as f:
-        content = f.read()
-        # Utiliser une expression régulière pour trouver le champ de date dans le frontmatter
-        match = re.search(
-            r"^---\s*\n.*?date: (\d{4}-\d{2}-\d{2}).*?\n---",
-            content,
-            re.DOTALL | re.MULTILINE,
-        )
+    # read and load content
+    with md_filepath.open(mode="r", encoding="UTF-8") as in_file:
+        content = frontmatter.load(in_file)
 
-        if match:
-            old_date = match.group(1)
-            new_date = old_date  # Même format, pas besoin de conversion ici
+    # manipulate date in YAML frontmatter
+    if content_date := content.metadata.get("date"):
+        if type(content_date) == date:
+            logging.debug(
+                f"File {md_filepath} has already a proper date defined in YAML "
+                "frontmatters."
+            )
+            continue
+        elif isinstance(content_date, str):
+            # parse date from str
+            try:
+                content.metadata["date"] = datetime.strptime(
+                    content_date, dt_format
+                ).date()
+            except ValueError:
+                logging.error(
+                    f"File {md_filepath} has a date in YAML frontmatters but it does "
+                    f"not comply with expected format ({dt_format}): {content_date}"
+                )
 
-            # Remplacer l'ancienne date par la nouvelle
-            updated_content = re.sub(
-                rf"^---\s*\n.*?date: {re.escape(old_date)}.*?\n---",
-                f"---\ndate: {new_date}\n---",
-                content,
-                flags=re.DOTALL | re.MULTILINE,
+            # write new version
+            with md_filepath.open(mode="w", encoding="UTF-8") as out_file:
+                # frontmatter.dump(content, out_file) # not working
+                out_file.write(frontmatter.dumps(content, sort_keys=False, indent=4))
+        else:
+            logging.warning(
+                f"File {md_filepath} has a date in YAML frontmatters but it's not a "
+                f"str: {content_date}"
             )
 
-            # Écrire le contenu mis à jour dans le fichier
-            with md_filepath.open(mode="w", encoding="UTF-8") as updated_file:
-                updated_file.write(updated_content)
+        continue
+
+    logging.info(f"File has no date defined in YAML frontmatters: {md_filepath}")
